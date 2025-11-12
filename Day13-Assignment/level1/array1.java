0101d48b00ffadea66b482ee4492a21eb241b8f7
vansh568 <aakhyagupta07@gmail.com>
2025-11-12 23:23:14 +0530

    Vansh-66[Assignment-13]

---
+++ Day13-Assignment/level1/array1.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class array1 {
+    public static void main(String [] args){
+        int [] arr = new in[4];
+        System.out.print(arr);
+    }
+}
---
+++ Day13-Assignment/level1/se_array1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class se_array1 {
+    public static void main(String [] args){
+        int[] arr= {1,2,3,4,5};
+        int x=5;
+        for(i=0;i<arr.length;i++){
+           if(arr[i]== x){
+            System.out.println("present");
+           }
+           else{
+            System.out.println("not present");
+           }
+        }
+    }
+        
+}
