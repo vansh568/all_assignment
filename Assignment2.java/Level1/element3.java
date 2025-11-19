d61749da33c9b0435fb0dfacfc10f2d103114618
vansh568 <aakhyagupta07@gmail.com>
2025-11-19 18:20:35 +0530

    Delete Assignment2.java directory

--- Assignment2.java/Level1/element3.java
+++
@@ -1,8 +0,0 @@
-public class element3 {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers * 0.621371;
-
-        System.out.println(kilometers + " kilometers is equal to " + miles + " miles");
-    }
-}
