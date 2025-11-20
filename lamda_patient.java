55257d29335a835824aa32bc5b821c857faab584
vansh568 <aakhyagupta07@gmail.com>
2025-11-20 13:35:47 +0530

    Delete lamda_patient.java

--- lamda_patient.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class lamda_patient {
-    public static void main(String[] args) {
-        List<Integer> patientIDs = Arrays.asList(101, 102, 103, 104, 105);
-        patientIDs.forEach(System.out::println);
-    }
-}
